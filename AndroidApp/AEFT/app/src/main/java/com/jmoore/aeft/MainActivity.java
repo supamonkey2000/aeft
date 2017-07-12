b6c4ed756592f464e274932f4d83903647966905
Joshua Moore <joshuamoore235@gmail.com>
2017-07-12 14:43:32 -0600

    Fixed secondary activity on app

--- AndroidApp/AEFT/app/src/main/java/com/jmoore/aeft/MainActivity.java
+++ AndroidApp/AEFT/app/src/main/java/com/jmoore/aeft/MainActivity.java
@@ -1,7 +1,11 @@
 package com.jmoore.aeft;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +13,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.BufferedInputStream;
 import java.io.File;
@@ -18,12 +23,12 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
 
-public class MainActivity extends AppCompatActivity {
-    public Uri selectedfile;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     public OutputStream out = null;
     public Socket socket = null;
     public File myFile = null;
     public byte[] buffer;
+    Button button;
 
 
     @Override
@@ -31,44 +36,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent()
-                .setType("*/*")
-                .setAction(Intent.ACTION_GET_CONTENT);
-
-        startActivityForResult(Intent.createChooser(intent, "Select a file"), 123);
-
-        Button buttonname;
-        buttonname = (Button) findViewById(R.id.sendB);
-
-        buttonname.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                myFile = new File(selectedfile.getPath());
-                buffer = new byte[(int) myFile.length()];
-
-                //NETWORK STUFF HERE
-                try {
-                    EditText et = (EditText) findViewById(R.id.editText3);
-                    String IP = et.getText().toString();
-                    socket = new Socket(IP, 25000);
-                    FileInputStream fis = new FileInputStream(myFile);
-                    BufferedInputStream in = new BufferedInputStream(fis);
-                    in.read(buffer, 0, buffer.length);
-                    out.flush();
-                    out.close();
-                    in.close();
-                    socket.close();
-                } catch(Exception ex) {ex.printStackTrace();}
-            }
-        });
+        button = (Button) findViewById(R.id.sendB);
+        button.setOnClickListener(this);
     }
 
-    @Override
+    public void onClick(View v) {
+        Intent i;
+        Toast.makeText(this,"INTENTNT CRETED",Toast.LENGTH_LONG).show();
+        i = new Intent(this, OpenFileActivity.class);
+        this.setContentView(R.layout.activity_open_file);
+        this.startActivityForResult(i, 2);//////////////////////////////////////////////////////////////////////////////////
+    }
+
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==123 && resultCode==RESULT_OK) {
-            selectedfile = data.getData(); //The uri with the location of the file
-            System.out.println(selectedfile);
+        EditText et = (EditText) findViewById(R.id.editText3);
+        if (resultCode == 2) {
+            String fileName = data.getStringExtra("fileName");
+            String shortFileName = data.getStringExtra("shortFileName");
+
+            Toast.makeText(this,
+                    "Selected File: " + fileName,
+                    Toast.LENGTH_SHORT).show();
+
+
+            et.setText(shortFileName);
+
+        } else {
+            Toast.makeText(this,
+                    "No File Selected, Cancel Or Back Pressed",
+                    Toast.LENGTH_SHORT).show();
+            et.setText("");
+        }
+
+        //NETWORK STUFF HERE
+        try {
+            String IP = et.getText().toString();
+            socket = new Socket(IP, 25000);
+            FileInputStream fis = new FileInputStream(myFile);
+            BufferedInputStream in = new BufferedInputStream(fis);
+            in.read(buffer, 0, buffer.length);
+            out.flush();
+            out.close();
+            in.close();
+            socket.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
 }
