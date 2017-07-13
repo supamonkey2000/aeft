fbf150e10552dab92c0c8532977cd35f10c0944f
Joshua Moore <joshuamoore235@gmail.com>
2017-07-13 10:03:46 -0600

    Code cleanup

--- AndroidApp/AEFT/app/src/main/java/com/jmoore/aeft/MainActivity.java
+++ AndroidApp/AEFT/app/src/main/java/com/jmoore/aeft/MainActivity.java
@@ -15,63 +15,62 @@ import java.io.FileInputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     Button button;
     public String IP;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.sendB);
+        button=(Button)findViewById(R.id.sendB);
         button.setOnClickListener(this);
     }
 
-    public void onClick(View v) {
-        EditText et = (EditText) findViewById(R.id.editText3);
-        IP = et.getText().toString();
+    public void onClick(View v){
+        EditText et=(EditText)findViewById(R.id.editText3);
+        IP=et.getText().toString();
         Intent i;
-        i = new Intent(this, OpenFileActivity.class);
+        i=new Intent(this,OpenFileActivity.class);
         this.setContentView(R.layout.activity_open_file);
-        this.startActivityForResult(i, 2);
+        this.startActivityForResult(i,2);
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        String fileName = null;
-        if (resultCode == 2) {
-            fileName = data.getStringExtra("fileName");
-        } else {
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        String fileName=null;
+        if(resultCode==2){
+            fileName=data.getStringExtra("fileName");
+        }else{
             Toast.makeText(this,"No File Selected, Cancel Or Back Pressed",Toast.LENGTH_SHORT).show();
         }
-        sendTheFile ooh = new sendTheFile();
+        sendTheFile ooh=new sendTheFile();
         ooh.execute(fileName,IP);
     }
 }
 
-class sendTheFile extends AsyncTask<String, Void, String> {
-    public OutputStream out = null;
-    public Socket socket = null;
-    public File myFile = null;
+class sendTheFile extends AsyncTask<String,Void,String>{
+    public OutputStream out=null;
+    public Socket socket=null;
+    public File myFile=null;
     public byte[] buffer;
     @Override
-    protected String doInBackground(String[] params) {
-        String IP = params[1];
-        myFile = new File(params[0]);
-        try {
-            socket = new Socket(IP, 25000);
-            FileInputStream fis = new FileInputStream(myFile);
-            buffer = new byte[(int) myFile.length()];
-            BufferedInputStream in = new BufferedInputStream(fis);
-            in.read(buffer, 0, buffer.length);
-            out = socket.getOutputStream();
+    protected String doInBackground(String[] params){
+        String IP=params[1];
+        myFile=new File(params[0]);
+        try{
+            socket=new Socket(IP,25000);
+            FileInputStream fis=new FileInputStream(myFile);
+            buffer=new byte[(int)myFile.length()];
+            BufferedInputStream in=new BufferedInputStream(fis);
+            in.read(buffer,0,buffer.length);
+            out=socket.getOutputStream();
             out.write(buffer,0,buffer.length);
             out.flush();
             out.close();
             in.close();
             socket.close();
-        } catch (Exception ex) {
+        }catch(Exception ex){
             ex.printStackTrace();
             Log.i("TAG","Failed to send file");
         }
--- AndroidApp/AEFT/app/src/main/java/com/jmoore/aeft/OpenFileActivity.java
+++ AndroidApp/AEFT/app/src/main/java/com/jmoore/aeft/OpenFileActivity.java
@@ -4,168 +4,116 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.AdapterView.OnItemLongClickListener;
-import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Spinner;
 import android.widget.Toast;
 
-public class OpenFileActivity extends Activity
-	implements OnClickListener, OnItemClickListener {
+public class OpenFileActivity extends Activity implements OnClickListener,OnItemClickListener{
 
 	ListView LvList;
-	
-	ArrayList<String> listItems = new ArrayList<String>();
-	
-	ArrayAdapter<String> adapter;
-	
+	ArrayList<String> listItems=new ArrayList<>();
+	ArrayAdapter<String>adapter;
 	Button BtnOK;
 	Button BtnCancel;
-	
-	String currentPath = null;
-	
-	public String selectedFilePath = null; /* Full path, i.e. /mnt/sdcard/folder/file.txt */
-	String selectedFileName = null; /* File Name Only, i.e file.txt */
+	String currentPath=null;
+	public String selectedFilePath=null;
+	String selectedFileName=null;
 	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_open_file);
 		
-		try {
-			/* Initializing Widgets */
-			LvList = (ListView) findViewById(R.id.LvList);
-			BtnOK = (Button) findViewById(R.id.BtnOK);
-			BtnCancel = (Button) findViewById(R.id.BtnCancel);
-			
-			/* Initializing Event Handlers */
-	        
+		try{
+			LvList=(ListView)findViewById(R.id.LvList);
+			BtnOK=(Button)findViewById(R.id.BtnOK);
+			BtnCancel=(Button)findViewById(R.id.BtnCancel);
 			LvList.setOnItemClickListener(this);
-			
 			BtnOK.setOnClickListener(this);
 			BtnCancel.setOnClickListener(this);
-			
-			//
-			
-			setCurrentPath(Environment.getExternalStorageDirectory().getAbsolutePath() + "/");
-		} catch (Exception ex) {
+			setCurrentPath(Environment.getExternalStorageDirectory().getAbsolutePath()+"/");
+		}catch(Exception ex){
 			Toast.makeText(this,
-					"Error in OpenFileActivity.onCreate: " + ex.getMessage(),
-					Toast.LENGTH_SHORT).show();
+					"Error in OpenFileActivity.onCreate: "+ex.getMessage(),Toast.LENGTH_SHORT).show();
 		}
 	}
 	
-	void setCurrentPath(String path) {
-		ArrayList<String> folders = new ArrayList<String>();
-		
-		ArrayList<String> files = new ArrayList<String>();
-		
-		currentPath = path;
-		
-		File[] allEntries = new File(path).listFiles();
+	void setCurrentPath(String path){
+		ArrayList<String>folders=new ArrayList<>();
+		ArrayList<String>files=new ArrayList<>();
+		currentPath=path;
+		File[] allEntries=new File(path).listFiles();
         
-	    for (int i = 0; i < allEntries.length; i++) {
-	    	if (allEntries[i].isDirectory()) {
+	    for (int i=0;i<allEntries.length;i++){
+	    	if(allEntries[i].isDirectory()){
 	    		folders.add(allEntries[i].getName());
-	    	} else if (allEntries[i].isFile()) {
+	    	}else if(allEntries[i].isFile()){
 	    		files.add(allEntries[i].getName());
 	    	}
 	    }
 	    
-	    Collections.sort(folders, new Comparator<String>() {
+	    Collections.sort(folders,new Comparator<String>(){
 	        @Override
-	        public int compare(String s1, String s2) {
+	        public int compare(String s1,String s2){
 	            return s1.compareToIgnoreCase(s2);
 	        }
 	    });
 	    
-	    Collections.sort(files, new Comparator<String>() {
+	    Collections.sort(files,new Comparator<String>(){
 	        @Override
-	        public int compare(String s1, String s2) {
+	        public int compare(String s1,String s2){
 	            return s1.compareToIgnoreCase(s2);
 	        }
 	    });
 	    
 	    listItems.clear();
 	    
-	    for (int i = 0; i < folders.size(); i++) {
-	    	listItems.add(folders.get(i) + "/");
+	    for (int i=0;i<folders.size();i++){
+	    	listItems.add(folders.get(i)+"/");
 	    }
 	    
-	    for (int i = 0; i < files.size(); i++) {
-	    	listItems.add(files.get(i));
-	    }
-        
-        adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,
-                listItems);
+	    for(int i=0;i<files.size();i++){
+            listItems.add(files.get(i));
+        }
+        adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,listItems);
         adapter.notifyDataSetChanged();
-        
         LvList.setAdapter(adapter);
 	}
 	
 	@Override
-	public void onBackPressed()
-	{
-	    if (!currentPath.equals(Environment.getExternalStorageDirectory().getAbsolutePath() + "/")) {
-	    	setCurrentPath(new File(currentPath).getParent() + "/");
-	    } else {
-		    super.onBackPressed();
-	    }
+	public void onBackPressed(){
+	    if(!currentPath.equals(Environment.getExternalStorageDirectory().getAbsolutePath()+"/")){
+	    	setCurrentPath(new File(currentPath).getParent()+"/");
+	    }else{super.onBackPressed();}
 	}
 	
 	@Override
-	public void onClick(View v) {
+	public void onClick(View v){
 		Intent intent;
-        //Toast.makeText(this, v.getTag().toString(),Toast.LENGTH_SHORT).show();
-        //switch (v.getId()) {
-		//case R.id.BtnOK:
-			
-		    intent = new Intent();
-            intent.putExtra("fileName", selectedFilePath);
-            intent.putExtra("shortFileName", selectedFileName);
-		    setResult(2, intent);
-		    
-			this.finish();
-			
-		//	break;
-		//case R.id.BtnCancel:
-		/*
-			intent = new Intent();
-		    intent.putExtra("fileName", "");
-		    intent.putExtra("shortFileName", "");
-		    setResult(RESULT_CANCELED, intent);
-		    
-			this.finish();
-			
-			break;
-		}*/
+		intent=new Intent();
+		intent.putExtra("fileName",selectedFilePath);
+        intent.putExtra("shortFileName",selectedFileName);
+		setResult(2,intent);
+		this.finish();
 	}
 
 	@Override
-	public void onItemClick(AdapterView<?> parent, View view, int position,
-			long id) {
-		String entryName = (String)parent.getItemAtPosition(position);
-		if (entryName.endsWith("/")) {
-			setCurrentPath(currentPath + entryName);
-		} else {
-			selectedFilePath = currentPath + entryName;
-			
-			selectedFileName = entryName;
-			
+	public void onItemClick(AdapterView<?>parent,View view,int position,long id){
+		String entryName=(String)parent.getItemAtPosition(position);
+		if(entryName.endsWith("/")){
+			setCurrentPath(currentPath+entryName);
+		}else{
+			selectedFilePath=currentPath+entryName;
+			selectedFileName=entryName;
 			this.setTitle(this.getResources().getString(R.string.title_activity_open_file)+"["+entryName+"]");
 		}
 	}
